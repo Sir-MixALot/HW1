c7ac5deac609ce14c51c74ab0e039dfa49cf2740
Sir-MixALot <sergey.gorshkov777@gmail.com>
2021-04-02 23:13:43 +0300

    The 3rd hw

--- index.php
+++ index.php
@@ -1,109 +1,33 @@
-<!-- The 1st task: -->
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-</head>
-<body>
 <?php
-$colors = ['white','blue','green'];
-$table = []; // array of numbers for table
-$k = rand(4, 10); // count of rows
-echo "<table>";
-for ($i = 1; $i<=$k; $i++){
-    $table[$i] = rand(0, 9);
-    echo "<tr><td>".$table[$i]."</td>";
-    echo "<td>".$table[$i]."</td></tr>";
+$text = 
+'What is Symfony. Symfony is a set of PHP Components, a Web Application framework, a Philosophy, and a Community - all working together in harmony. 
+Symfony Framework. The leading PHP framework to create websites and web applications. Built on top of the Symfony Components. 
+Symfony Components. A set of decoupled and reusable components on which the best PHP applications are built, such as Drupal, phpBB, and eZ Publish. 
+Symfony Community. A passionate group of over 600,000 developers from more than 120 countries, all committed to helping PHP surpass the impossible. 
+Symfony Philosophy. Embracing and promoting professionalism, best practices, standardization and interoperability of applications.';
+echo str_replace('Symfony','<span style="color:#cc34eb">Symfony</span>', "<p style='font-size:30px'>" . $text . "</p>");
+echo "<h1>The number of occurrences of *Symfony* in the text is: " . substr_count($text, 'Symfony', 0) . "</h1>";
+$symbols = [".", ",", "\n"];
+$p_count = explode("\n", $text);
+echo "<h1>The number of all paragraphs in the text is: " . count($p_count) . "</h1>";
+$s_count = explode(". ", $text);
+echo "<h1>The number of all sentences in the text is: " . count($s_count) . "</h1>";
+echo "<h1>The number of all words in the text is: " . str_word_count($text) . "</h1>";
+echo "<h1>The number of all symbols in the text is: " . strlen($text) . " (with gaps)</h1>";
+$new_text2 = str_replace(" ", '', $text);
+echo "<h1>The number of all symbols in the text is: " . strlen($new_text2) . " (without gaps)</h1>";
+$new_text3 = str_replace($symbols, '', $text);
+$word = explode(" ", $new_text3);
+$tmp = array_map(function ($item) { return mb_strlen($item); }, $word);
+$maxs = array_filter($word, function($item) use ($tmp) { return mb_strlen($item) == max($tmp); });
+foreach($maxs as $value){
+    echo "<h1> The longest word is: <span style='color:#cc34eb'>" . $value . "</span></h1>";
 }
-echo "</table><br><hr>";
-?>
-</body>
-</html>
-
-<!-- The 2nd task: -->
-<?php
-echo 'The 2nd task: <br>';
-$arr = [];
-$x = rand(4, 9);//array dimension
-for ($i=1;$i<=$x;$i++){
-    $arr[$i] = rand(1, 9)+($i+2);
-    echo $arr[$i] . "_";
+$all_symbols =[];
+foreach (count_chars($text, 1) as $i => $value) {
+    $all_symbols[chr($i)] = $value;
 }
-$min = $max = $arr[1]; //min and max elements
-$imin = 1; $imax = 0; //keys of min and max elements
-$result = 1; //product of numbers
-foreach ($arr as $key=>$a){
-    if($min>$a){//the first minimum number is taken
-        $min = $a;
-        $imin = $key;
-    }
-    if($max<=$a){//the last maximum number is taken
-        $max = $a;
-        $imax = $key;
-    }
-}
-for($j = $imin+1;$j<$imax;$j++){
-    $result *= $arr[$j];
-}
-echo "<br>Result: " . $result;
-echo "<br><hr>";
-
-//The 3rd task:
-echo 'The 3rd task: <br>';
-$arr = [];//the main array
-$arr1 = [];//an array of odd numbers
-$arr2 = [];//an array of even numbers
-$arr3 = [];//the final array
-$x = rand(4, 9);//$arr dimension
-for ($i=0;$i<=$x;$i++){
-    $arr[$i] = rand(1, 9);
-    echo $arr[$i] . "_";
-}
-echo "<br>";
-foreach($arr as $key=>$a){
-    if(($key == 0)||($key % 2 == 0)){
-        $arr2[$key]=$a;
-    }
-    else{
-        $arr1[$key]=$a;
-    }
-}
-$arr3 = $arr1+$arr2;
-foreach($arr3 as $a3){
-    echo $a3 . "_";
-}
-echo "<br><hr>";
-
-//The 4th task:
-echo 'The 4th task: <br>';
-$matrix=[];
-$x = rand(1, 5);//$arr dimension
-$y = rand(1,5);//$arr dimension
-for($i=0;$i<$x;$i++){
-    for($j=0;$j<$y;$j++){
-        $matrix[$i][$j] = rand(0, 9);
-    }
-}
-echo "<table>";
-foreach($matrix as $m){
-    echo "<tr>";
-    foreach($m as $m_m){
-        echo "<td>{$m_m}</td>";
-    }
-    echo "</tr>";
-}
-echo "</table>";
-for($j=0;$j<$y;$j++){
-    $column = array_column($matrix, $j);
-    if (in_array(0,$column)){
-        echo "There is zero in this column:". $j+1 . "<br>";
-    } 
-    else {
-        $pr = 1;
-        foreach($column as $c){
-            $pr*=$c;
-        }
-        echo "The product of the numbers in column ". $j+1 ." is: " . $pr . "<br>";
-    }
+natcasesort($all_symbols); 
+foreach ($all_symbols as $key => $value) {
+    echo "<h2>\"" , $key , "\" occurs $value time(s) in a line.</h2>";
 }
